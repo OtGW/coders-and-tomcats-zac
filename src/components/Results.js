b2422c0110341a1605765312a49475e42d6e4f00
Zac Luchette <luchettezac@gmail.com>
2023-02-02 19:00:02 -0500

    Work on Results nd ResultsDisplay

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,10 +1,27 @@
 import React from "react"
 import { database, useAuthState } from "../hooks/Firebase"
+import ResultsDisplay from "./ResultsDisplay.js"
 
 // TODO: Finish the Results component+functionality
 
-const Results = (contestNumber) => {
+const Results = ({ contestNumber }) => {
   const { data } = useAuthState()
+
+  const contestOver = () => {
+    const timeLeft = () => {
+      const currentTime = Date.now()
+      console.log(currentTime)
+      const endOfContest = new Date(1669914000)
+      console.log(endOfContest)
+      return endOfContest - currentTime
+    }
+    if (timeLeft > 0) {
+      return false
+    } else {
+      return true
+    }
+  }
+
   let coderWin = 0
   let tomcatWin = 0
   for (let i = 0; i < data.contests.length; i++) {
@@ -23,11 +40,15 @@ const Results = (contestNumber) => {
   }
 
   if (!data[contestNumber].open) {
-    return <div> This contest has ended the winner is {Winner()}</div>
+    // return <div> This contest has ended the winner is {Winner()}</div>
   } else {
     return {
       /*the current score and time left*/
     }
+  }
+
+  if (contestOver) {
+    return <ResultsDisplay winner={Winner()} />
   }
 }
 
---
+++ src/components/ResultsDisplay.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react"
+import { Modal } from "@mantine/core"
+import { ref, update, push, increment } from "firebase/database"
+import { database, useAuthState } from "../hooks/Firebase"
+
+const ResultsDisplay = ({ winner }) => {
+  const [opened, setOpened] = useState(true)
+
+  return (
+    <Modal
+      centered
+      opened={opened}
+      size="auto"
+      onClose={() => setOpened(false)}
+    >
+      This contest has ended. The winner is {winner}!
+      {/*image depending on winner, sound, confetti effect*/}
+    </Modal>
+  )
+
+  //event listener: detect new login, setOpened(true)
+}
+
+export default ResultsDisplay
+
+/*
+TODO: Dumb component - pop up box that displays from Results.js. Start w/
+just simple pop up, possibly using useRef? But eventually more visually pleasing/
+funny pop up w/ visuals representing winning team, sound effect and confetti.
+*/
+//TODO: Should this be a useEffect in Results.js instead?
